4f405e5895e49e276b3ead4ca696bb7930e2759a
Yead <121762721+yead-study@users.noreply.github.com>
2023-02-27 15:41:01 +0600

    Added header.h and edited main function

---
+++ header.h
@@ -0,0 +1,7 @@
+#ifndef SYM-TASKER_HEADER_H
+#define SYM-TASKER_HEADER_H
+
+void help();
+void version();
+
+#endif //SYM-TASKER_HEADER_H
--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
 #include<iostream>
 #include<vector>
+#include"header.h"
 
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
