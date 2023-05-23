3537efc4f100ac6c377cf1d7fbbd7ade087d22f5
Yead <119938455+yeadkhail@users.noreply.github.com>
2023-05-23 09:58:52 +0600

    Fixed minor mistakes.

--- functions/version.cpp
+++ functions/version.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void version()
 {
-    cout<<"sym tasker "<<"(Symple Utilities) "<<"v0.0.1"<<endl;
+    cout<<"sym tasker "<<"(Symple Utilities) "<<"v1.0.1"<<endl;
     cout<<"Copyright (C) 2023 Team Symple"<<endl;
     cout<<"License GPLv3+: GNU version 3 or later <https://gnu.org/licences/gpl.html>."<<endl<<"This is a free software: You are free to change and redestribute it."<<endl;
     cout<<"There is NO WARRANTY, to the extent permitted by law."<<endl<<endl;
